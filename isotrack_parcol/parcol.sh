a8dda4aa4ad42bae68f7a9e5b67f42ffed7326bd
yang.chen@unipd.it <cycyustc@gmail.com>
2017-06-13 12:31:14 +0200

    correct copying Mdot
    Former-commit-id: a2a71abe9143f83a42087dbc267222662506a665

--- isotrack_parcol/parcol.sh
+++ isotrack_parcol/parcol.sh
@@ -1,4 +1,4 @@
 python ../revisegrid/dbert_colibri_replace.py ./S_EAGB_colibri.list > log.txt
-../revisegrid/revisegrid_comp.pl    ../revisegrid/main    CAF09_V1.2S_M36_S12D_NS_MAS3    S12_    CAF09_V1.2S_M36_S12D_NS_MAS3_parcol_comp.dat
+../revisegrid/revisegrid_comp.pl    ../revisegrid/main    CAF09_V1.2S_M36_S12D_NS_MAS3    S12_    CAF09_V1.2S_M36_S12D_NS_MAS3_parcol_comp.dat > log1.txt
 sed -i 's/isotrack\/parsec/isotrack\/parcol/g' CAF09_V1.2S_M36_S12D_NS_MAS3_parcol_comp.dat > log0.txt
 #../revisegrid/main tmp CAF09_V1.2S_M36_S12D_NS_MAS3_parcol_comp.dat 0.04 0.01 > log0.txt
